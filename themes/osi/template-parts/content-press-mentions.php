8479d1468dabd8db9c5985f0319e9ed3b99410eb
Bernat <bernattorras@gmail.com>
2023-08-24 17:07:15 +0200

    Make press-mention's title linkable in the single page

---
+++ themes/osi/template-parts/content-press-mentions.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Template part for displaying press mentions
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package osi
+ */
+
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<?php get_template_part( 'template-parts/header-featured-image' ); ?>
+
+	<div class="entry-content post--content">
+		<?php if ( get_post_type() !== sugar_calendar_get_event_post_type_id() ) { ?>
+
+			<div class="entry-meta post--byline">
+				<?php osi_posted_on(); ?>
+				<?php echo wp_kses_post( osi_get_single_taxonomy_terms_links( $post, 'category' ) ); ?>
+			</div><!-- .entry-meta -->
+
+		<?php } ?>
+		<?php 
+		$title = get_field( 'article_url' ) ?
+		sprintf( '<a href="%s" rel="bookmark" target="_blank">%s</a>',
+			esc_url( get_field('article_url') ),
+			esc_html( get_the_title() )
+		) :
+		esc_html( get_the_title() );
+		
+		echo '<h1 class="entry-title post--title">'.$title.'</h1>';
+		?>
+
+		<?php the_content(); ?>
+	</div><!-- .entry-content -->
+	<section id="pre-footer">
+		<?php get_template_part( 'template-parts/nav-postname-pager' ); ?>
+		<?php //get_template_part( 'template-parts/email-block' ); ?>
+	</section>
+
+</article><!-- #post-<?php the_ID(); ?> -->
