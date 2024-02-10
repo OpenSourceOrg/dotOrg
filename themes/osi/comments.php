d2c051b0ca006b20d508710e4cca5a5d3256dc62
Nick Green <nick.green@automattic.com>
2024-02-09 17:21:33 -0800

    Remove default WP comments
    But still leave the comment form for web mentions

--- themes/osi/comments.php
+++ themes/osi/comments.php
@@ -22,56 +22,6 @@ if ( post_password_required() ) {
 
 <div id="comments" class="comments comments-area">
 
-	<?php
-	// You can start editing here -- including this comment!
-	if ( have_comments() ) :
-		?>
-		<h3 class="comments--title">
-			<?php
-			$comment_count = get_comments_number();
-			if ( 1 === $comment_count ) {
-				printf(
-					/* translators: 1: title. */
-					esc_html_e( 'One thought on &ldquo;%1$s&rdquo;', 'osi' ),
-					'<span>' . esc_html( get_the_title() ) . '</span>'
-				);
-			} else {
-				printf( // phpcs:ignore
-					/* translators: 1: comment count number, 2: title. */
-					esc_html( _nx( '%1$s thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', $comment_count, 'comments title', 'osi' ) ),
-					esc_html( number_format_i18n( $comment_count ) ),
-					'<span>' . esc_html( get_the_title() ) . '</span>'
-				);
-			}
-			?>
-		</h2><!-- .comments-title -->
-
-		<?php the_comments_navigation(); ?>
-
-		<ol class="comments--list">
-			<?php
-				wp_list_comments(
-					array(
-						'style'      => 'ol',
-						'short_ping' => true,
-					)
-				);
-			?>
-		</ol><!-- .comment-list -->
-
-		<?php
-		the_comments_navigation();
-
-		// If comments are closed and there are comments, let's leave a little note, shall we?
-		if ( ! comments_open() ) :
-			?>
-			<p class="no-comments alert alert-warning"><?php esc_html_e( 'Comments are closed.', 'osi' ); ?></p>
-			<?php
-		endif;
-
-	endif; // Check for have_comments().
-
-	comment_form();
-	?>
+	<?php comment_form(); ?>
 
 </div><!-- #comments -->
