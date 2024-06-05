1d2dccf7edadf5b5094a2731f7841bba409bc8df
Bernat <bernattorras@gmail.com>
2024-06-05 11:30:49 +0200

    Disable header for posts/events without featured image

--- themes/osi/template-parts/header-featured-image.php
+++ themes/osi/template-parts/header-featured-image.php
@@ -1,6 +1,6 @@
 <?php
-if ( function_exists( 'Sugar_Calendar\AddOn\Ticketing\Settings\get_setting' ) ) {
 
+if ( function_exists( 'Sugar_Calendar\AddOn\Ticketing\Settings\get_setting' ) ) {
 	$recepit_page_id = Sugar_Calendar\AddOn\Ticketing\Settings\get_setting( 'receipt_page' );
 
 	if ( get_the_ID() === absint( $recepit_page_id ) ) {
@@ -22,11 +22,19 @@ if ( ! isset( $page_title ) ) {
 			</div>
 		</div>
 	</header>
-<?php } elseif ( ! has_post_thumbnail() && ! in_array( get_post_type( $post ), array( 'post', 'event' ), true ) ) { ?>
+<?php } else if ( ! has_post_thumbnail() && ! in_array( get_post_type( $post ), array( 'post', 'event' ), true ) ) { ?>
 	<header class="entry-header cover--header no-thumbnail">
 		<div class="wp-block-cover alignfull">
 			<div class="wp-block-cover__inner-container">
-				<?php // echo ( ! empty( $page_title ) ) ? '<h1 class="entry-title page--title">' . esc_html( $page_title ) . '</h1>' : ''; ?>
+				<?php osi_the_page_dates(); ?>
+			</div>
+		</div>
+	</header>
+<?php } elseif ( is_page() ) { ?>
+	<header class="entry-header cover--header no-thumbnail">
+		<div class="wp-block-cover alignfull has-neutral-dark-background-color has-background-dim-100 has-background-dim">
+			<div class="wp-block-cover__inner-container">
+				<?php echo ( ! empty( $page_title ) ) ? '<h1 class="entry-title page--title">' . esc_html( $page_title ) . '</h1>' : ''; ?>
 				<?php osi_the_page_dates(); ?>
 			</div>
 		</div>
