ac3553c07418961628f3af646b8499627c346e1c
Geoff <geoff.guillain@automattic.com>
2024-08-27 11:22:23 -1000

    linting errors

--- themes/osi/single.php
+++ themes/osi/single.php
@@ -19,12 +19,10 @@ get_header(); ?>
 			} else {
 				get_template_part( 'template-parts/breadcrumbs' );
 			}
-			
+
 			while ( have_posts() ) :
 				the_post();
-
 				get_template_part( 'template-parts/content', get_post_type() );
-
 			endwhile; // End of the loop.
 			?>
 		</section>
