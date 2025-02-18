cb2b954a7e751ad90f15846acdf7cf56985f07e7
Ariel Jolo <ajolo@sysarmy.com>
2025-02-18 02:12:13 -0300

    tidying up styles

--- themes/osi/templates/ai-fse.php
+++ themes/osi/templates/ai-fse.php
@@ -475,6 +475,7 @@ Description: Custom landing page template with Full Site Editing (FSE) capabilit
     <script src="https://opensourceorg.github.io/ai/assets/js/plugins/counter-up.js"></script>
     <script src="https://opensourceorg.github.io/ai/assets/js/plugins/swiper.js"></script>
     <script src="https://opensourceorg.github.io/ai/assets/js/vendor/waw.js"></script>
+    <script src="https://opensourceorg.github.io/ai/assets/js/vendor/waypoint.js"></script>
     <script src="https://opensourceorg.github.io/ai/assets/js/plugins/scrolltigger.js"></script>
     <script src="https://opensourceorg.github.io/ai/assets/js/plugins/bootstrap.min.js"></script>
     <script src="https://opensourceorg.github.io/ai/assets/js/main.js"></script>
