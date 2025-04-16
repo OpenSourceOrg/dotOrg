30041f17cd252080d7233d60aa0bc393c454fee5
Nicholas Pagazani <41761220+nickpagz@users.noreply.github.com>
2025-04-16 11:01:51 +0200

    Make board member statuses not public

--- plugins/osi-features/inc/classes/taxonomies/class-taxonomy-status.php
+++ plugins/osi-features/inc/classes/taxonomies/class-taxonomy-status.php
@@ -68,11 +68,18 @@ class Taxonomy_Status extends Base {
 	 * @return array
 	 */
 	public function get_args() {
-
 		return wp_parse_args(
 			array(
-				'hierarchical' => false,
-				'rewrite'      => array( 'slug' => 'status' ),
+				'hierarchical'       => false,
+				'rewrite'            => false,
+				'public'             => false,
+				'publicly_queryable' => false,
+				'show_ui'            => true,
+				'show_in_menu'       => true,
+				'show_in_nav_menus'  => false,
+				'show_in_rest'       => true,
+				'show_admin_column'  => true,
+				'query_var'          => true,
 			),
 			parent::get_args()
 		);
