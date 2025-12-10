cc9dcaec61f4715a3a37a9c40015a9b542bd8095
Yatin <yatin@pixel21.com>
2025-12-10 11:38:00 -0600

    Fix for broken templates after 6.9 update

--- themes/osi/functions.php
+++ themes/osi/functions.php
@@ -584,3 +584,10 @@ function osi_enqueue_swiper_assets(): void {
 	wp_enqueue_script( 'swiper-js', 'https://unpkg.com/swiper@11/swiper-bundle.min.js', array(), filemtime( untrailingslashit( get_template_directory() ) . '/style.css' ), true );
 }
 add_action( 'wp_enqueue_scripts', 'osi_enqueue_swiper_assets' );
+
+/**
+ * Disable loading separate block styles for the osi theme.
+ *
+ * @return bool
+ */
+add_filter( 'should_load_separate_core_block_assets', '__return_false' );
